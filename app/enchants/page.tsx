67527df8e2e94a7adeafec5c3eb8e4f03755df89
ooty224 <phallac8298@gmail.com>
2026-07-25 04:49:34 +0900

    Add files via upload

---
+++ app/enchants/page.tsx
@@ -0,0 +1,14 @@
+export default function EnchantsPage() {
+  return (
+    <iframe
+      src="/enchants.html"
+      title="Dexxert 인첸트 도감"
+      style={{
+        display: "block",
+        width: "100%",
+        height: "100dvh",
+        border: 0,
+      }}
+    />
+  );
+}
